74be0cfbbee6022f909d81cec1997a0f4567ab81
Lawrence Paine <60822247+LawrencePaine@users.noreply.github.com>
2024-04-30 15:16:40 +0100

    adds additional components

---
+++ src/components/pages/contact.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './home.css';
+
+const Contact: React.FC = () => {
+  return (
+    <>
+      <div className="section-container">
+        <div className="section-content">
+          <div className="image-wrapper">
+            <img src="/images/about2.jpg" alt="About 2" className="section-image" />
+          </div>
+          <div className="text-content">
+            <h2>Section Title 2</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel ipsum id nulla tristique tincidunt. Cras vel condimentum sapien, id faucibus quam.</p>
+          </div>
+        </div>
+      </div>
+      <div className="section-container">
+        <div className="section-content">
+          <div className="image-wrapper">
+            <img src="/images/about3.jpg" alt="About 3" className="section-image" />
+          </div>
+          <div className="text-content">
+            <h2>Section Title 3</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel ipsum id nulla tristique tincidunt. Cras vel condimentum sapien, id faucibus quam.</p>
+          </div>
+        </div>
+      </div>
+      <div className="section-container">
+        <div className="section-content">
+          <div className="image-wrapper">
+            <img src="/images/about4.jpg" alt="About 4" className="section-image" />
+          </div>
+          <div className="text-content">
+            <h2>Section Title 4</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel ipsum id nulla tristique tincidunt. Cras vel condimentum sapien, id faucibus quam.</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
